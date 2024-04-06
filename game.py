ad98d0bbb95545967ee2a9b044d2727151db509e
BrownSheepie2021 <86570972+BrownSheepie2021@users.noreply.github.com>
2024-04-06 00:14:26 -0400

    Added custom level editor

--- game.py
+++ game.py
@@ -1,21 +1,22 @@
 import sys
 import pygame
-from scripts.utils import load_image, load_images
-from scripts.entities import PhysicsEntity
+from scripts.utils import load_image, load_images, Animation
+from scripts.entities import Player
 from scripts.tilemap import Tilemap
+from scripts.clouds import Clouds
 
 class Game:
     def __init__(self):
         pygame.init()
 
-        pygame.display.set_caption('ninja game')
+        pygame.display.set_caption('Ninja Game')
         self.screen = pygame.display.set_mode((640, 480))
         self.display = pygame.Surface((320, 240))
 
         self.clock = pygame.time.Clock()
-        
+
         self.movement = [False, False]
-        
+
         self.assets = {
             'decor': load_images('tiles/decor'),
             'grass': load_images('tiles/grass'),
@@ -23,26 +24,38 @@ class Game:
             'stone': load_images('tiles/stone'),
             'player': load_image('entities/player.png'),
             'background': load_image('background.png'),
+            'clouds': load_images('clouds'),
+            'player/idle': Animation(load_images('entities/player/idle'), img_dur=6),
+            'player/run': Animation(load_images('entities/player/run'), img_dur=4),
+            'player/jump': Animation(load_images('entities/player/jump')),
+            'player/slide': Animation(load_images('entities/player/slide')),
+            'player/wall_slide': Animation(load_images('entities/player/wall_slide')),
         }
-        
-        self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
-        
+
+        self.clouds = Clouds(self.assets['clouds'], count=16)
+
+        self.player = Player(self, (50, 50), (8, 15))
+
         self.tilemap = Tilemap(self, tile_size=16)
+        self.tilemap.load('map.json')
 
         self.scroll = [0, 0]
         
     def run(self):
         while True:
             self.display.blit(self.assets['background'], (0, 0))
-
+            
             self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 30
             self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
             render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
-
+            
+            self.clouds.update()
+            self.clouds.render(self.display, offset=render_scroll)
+            
             self.tilemap.render(self.display, offset=render_scroll)
             
             self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
-            self.player.render(self.display)
+            self.player.render(self.display, offset=render_scroll)
             
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
